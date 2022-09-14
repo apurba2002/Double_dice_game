4622686357a38616066595f5fee45de815ceb3fc
apurba2002 <debnathapurba582@gmail.com>
2022-09-14 19:53:41 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Enter the first player name :");
+string player1 = Console.ReadLine();
+Console.WriteLine("Enter the second player name :");
+string player2 = Console.ReadLine();
+    Random rd = new Random();
+    int p1d1 = rd.Next(0,7);
+    int p2d1 = rd.Next(0,7);
+    if (p1d1 > p2d1 )
+    {
+        Console.WriteLine("The winner is :");
+        Console.WriteLine(player1);
+        continue;
+    }
+    if ( p2d1 > p1d1)
+    {
+        Console.WriteLine("The winner is :");
+        Console.WriteLine(player2);
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Match draw");
+        continue;
+    }
+}
