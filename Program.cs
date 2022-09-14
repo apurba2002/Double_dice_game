f17bcc12d1585ebd3c687e0ba27e690ae4a6ec05
apurba2002 <62664361+apurba2002@users.noreply.github.com>
2022-09-14 19:55:41 +0600

    line indexing correction

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 while (true)
 {
     Console.WriteLine("Enter the first player name :");
-string player1 = Console.ReadLine();
-Console.WriteLine("Enter the second player name :");
-string player2 = Console.ReadLine();
+    string player1 = Console.ReadLine();
+    Console.WriteLine("Enter the second player name :");
+    string player2 = Console.ReadLine();
     Random rd = new Random();
     int p1d1 = rd.Next(0,7);
     int p2d1 = rd.Next(0,7);
